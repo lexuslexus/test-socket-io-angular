225287b409921d366639b14aaec1425bc91af677
unknown <lexus.lin@LEXUS-LIN.corp.vipshop.com>
2014-06-13 01:01:10 +0800

    f

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var express = require('express'),
 	port = process.env.PORT || 3000;
 
 
-var app = express();
+var app = express(),
+	messageList = [];
+
 app.use(express.static(__dirname + '/static'));
 
 app.use(function (req, res) {
@@ -18,6 +20,15 @@ var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function(socket){
 	socket.emit('connected');
+})
+
+io.sockets.on('send', function(message){
+	messageList.push(message);
+	socket.emit('sent', message);
+})
+
+io.sockets.on('getAllMessage', function(message){
+	socket.emit('sendAllMessage', messageList);
 })
 
 console.log('TechNode is on port ' + port + '!');
